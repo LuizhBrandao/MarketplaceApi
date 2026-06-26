a4af9c8fc972f24c653547c0b3251b859bf4cd08
LuizhBrandao <luizh.brandaoo@gmail.com>
2026-06-26 16:07:52 -0300

    feat: implementa repositorio em memoria para produtos

---
+++ MarketplaceApi.ApiService/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using MarketplaceApi.ApiService.Interfaces;
+using MarketplaceApi.ApiService.Models;
+
+namespace MarketplaceApi.ApiService.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    // Utilizamos uma lista privada em memória para simular o banco de dados
+    private readonly List<Product> _products = new();
+
+    public IEnumerable<Product> GetAll()
+    {
+        return _products;
+    }
+
+    public void Add(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public void Delete(Guid id)
+    {
+        // Remove todos os produtos que tenham o Id correspondente (neste caso, será apenas um)
+        _products.RemoveAll(p => p.Id == id);
+    }
+}
